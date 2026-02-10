9ac88e455cd0f4655a969bb3f1f4bc92ba2a9af5
Ayuba Sherif Deen <ayubasherifdeen@gmail.com>
2026-02-10 19:56:04 +0000

    feature to change password

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,7 @@
 """Defines urls for users"""
 from django.urls import path, include
 from . import views
-from django.contrib.auth.views import (
-    PasswordResetView,
-    PasswordResetDoneView,
-    PasswordResetConfirmView,
-    PasswordResetCompleteView
-)
+from django.contrib.auth import views as auth_views
 
 app_name='users'
 urlpatterns=[
@@ -14,12 +9,5 @@ urlpatterns=[
     path('', include('django.contrib.auth.urls')),
         #Registration page
     path('register/', views.register, name='register'),
-        #Reset form
-    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-        #Reset link
-    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-        #New password form
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-        #Reset completion confirmation
-    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    
 ]
